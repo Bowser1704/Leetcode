2ac6c6d78e02aac46d58e48f88a5b581a6c78a24
Bowser1704 <yuqlang@163.com>
2020-04-23 21:06:31 +0800

    dp

---
+++ 300.最长上升子序列.java
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长上升子序列
+ *
+ * https://leetcode-cn.com/problems/longest-increasing-subsequence/description/
+ *
+ * algorithms
+ * Medium (44.16%)
+ * Likes:    654
+ * Dislikes: 0
+ * Total Accepted:    89.7K
+ * Total Submissions: 203.1K
+ * Testcase Example:  '[10,9,2,5,3,7,101,18]'
+ *
+ * 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ * 
+ * 示例:
+ * 
+ * 输入: [10,9,2,5,3,7,101,18]
+ * 输出: 4 
+ * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ * 
+ * 说明:
+ * 
+ * 
+ * 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+ * 你算法的时间复杂度应该为 O(n^2) 。
+ * 
+ * 
+ * 进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] top = new int[nums.length];
+        // 牌堆数初始化为 0, piles 是从 1 开始的, 但是 top 的 index 是从 0 开始的。
+        int piles = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 要处理的扑克牌
+            int poker = nums[i];
+    
+            /***** 搜索左侧边界的二分查找 *****/
+            int left = 0, right = piles;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] > poker) {
+                    right = mid;
+                } else if (top[mid] < poker) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            /*********************************/
+    
+            // 没找到合适的牌堆，新建一堆
+            if (left == piles) piles++;
+            // 把这张牌放到牌堆顶
+            top[left] = poker;
+        }
+        // 牌堆数就是 LIS 长度
+        return piles;
+    }
+}
+// @lc code=end
+
---
+++ 322.零钱兑换.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=322 lang=java
+ *
+ * [322] 零钱兑换
+ *
+ * https://leetcode-cn.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (39.05%)
+ * Likes:    558
+ * Dislikes: 0
+ * Total Accepted:    76.9K
+ * Total Submissions: 196.9K
+ * Testcase Example:  '[1,2,5]\n11'
+ *
+ * 给定不同面额的硬币 coins 和一个总金额
+ * amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入: coins = [1, 2, 5], amount = 11
+ * 输出: 3 
+ * 解释: 11 = 5 + 5 + 1
+ * 
+ * 示例 2:
+ * 
+ * 输入: coins = [2], amount = 3
+ * 输出: -1
+ * 
+ * 
+ * 
+ * 说明:
+ * 你可以认为每种硬币的数量是无限的。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < dp.length; i++) {
+            for (int coin : coins) {
+                if (i < coin) continue;
+                dp[i] = Math.min(1 + dp[i - coin], dp[i]);
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+
+}
+// @lc code=end
+
